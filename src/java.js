e73e2d010bd654fbcbf2743c698a65e7cd2d01d4
Valeriuna <106536158+Valeriuna@users.noreply.github.com>
2022-07-23 12:51:57 +0300

    update forecast API

--- src/java.js
+++ src/java.js
@@ -143,23 +143,39 @@ let celsiusLink = document.querySelector("#celcium");
 celsiusLink.addEventListener("click", convertToCelsius);
 
 ///
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
+
 function displayForecast(response) {
-  console.log(response.data.daily);
+  let forecast = response.data.daily;
   let forecastElement = document.querySelector("#forecast");
 
-  let days = ["MON", "TUE", "WED", "THU", "FRI"];
   let forecastHTML = `<div class="row">`;
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `  
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 5) {
+      forecastHTML =
+        forecastHTML +
+        `  
             <div class="col">
-              <div class="days">${day}</div>
-              <img src="http://openweathermap.org/img/wn/01d@2x.png" alt="" width="40">
-              <div class="temp">19°C | 6°C</div>
+              <div class="days">${formatDay(forecastDay.dt)}</div>
+              <img src="http://openweathermap.org/img/wn/${
+                forecastDay.weather[0].icon
+              }@2x.png" alt="" width="40">
+              <div class="temp">
+              <span class="max-temp">
+                ${Math.round(forecastDay.temp.max)}°C | </span>
+              <span class="min-temp">
+                ${Math.round(forecastDay.temp.min)}°C </span>
+              </div>
             </div>
             
   `;
+    }
   });
 
   forecastHTML = forecastHTML + `</div>`;
